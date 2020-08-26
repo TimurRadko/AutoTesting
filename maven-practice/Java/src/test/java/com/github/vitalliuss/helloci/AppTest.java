8cb4fc6e0825a1eef53e635db761fef8a8ebf645
Timur Radko <timurradko@gmail.com>
2020-08-26 21:04:45 +0300

    add maven training

---
+++ maven-practice/Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -0,0 +1,40 @@
+package com.github.vitalliuss.helloci;
+
+
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ * @author vitali_shulha
+ */
+public class AppTest {
+
+	@Test
+	public void testShouldBePassed() {
+		assertTrue(true);
+	}
+
+	//If we change "assertTrue" on false, so we will fail tests
+	@Test
+	public void testShouldBeFailed() {
+		assertTrue(true);
+	}
+
+	@Ignore("Not implemented yet")
+	@Test
+	public void testShouldBeSKipped() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testAnotherMethod() {
+		App.doNothing();
+	}
+	
+	@Test
+	public void testAnotherClass() {
+		EmptyClass.emptyMethod();
+	}
+}
